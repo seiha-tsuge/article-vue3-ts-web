ea9b4019f0ea159b338f0d0f870b4c070be42900
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-01-24 15:20:06 +0900

    feat: 新規作成

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest', 'cypress'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended',
+    'prettier',
+    'prettier/vue',
+    'prettier/@typescript-eslint'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
