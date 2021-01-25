aa661aba7354e2e96049a056967f64bd47814e3e
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-01-25 22:11:57 +0900

    feat: ESLintを設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
